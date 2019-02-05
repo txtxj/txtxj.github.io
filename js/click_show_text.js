c8b29801d13a58d5afd778586080da97fbb44155
txtxj <1418373194@qq.com>
2019-02-05 18:24:55 +0800

    Site updated: 2019-02-05 18:24:54

--- js/click_show_text.js
+++ js/click_show_text.js
@@ -2,7 +2,7 @@ var a_idx = 0;
 jQuery(document).ready(function($) {
     $("body").click(function(e) {
         var a = new Array
-        ("��ǿ", "����", "����", "��г", "����", "ƽ��", "����", "����", "����", "��ҵ", "����", "����");
+        ("富强", "民主", "文明", "和谐", "自由", "平等", "公正", "法治", "爱国", "敬业", "诚信", "友善");
         var $i = $("<span/>").text(a[a_idx]);
         a_idx = (a_idx + 1) % a.length;
         var x = e.pageX,
