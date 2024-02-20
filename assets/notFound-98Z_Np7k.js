cf5dfa00224fb1c4ffc043d0dc1a17fb5e8520de
txtxj <1418373194@qq.com>
2024-02-21 00:49:26 +0800

    use vue test 5

--- assets/notFound-98Z_Np7k.js
+++
@@ -1,1 +0,0 @@
-import{J as o,$ as n,z as r,y as t}from"./index-iPrAVeB0.js";const c={};function s(_,a){const e=n("router-view");return t(),r(e)}const p=o(c,[["render",s]]);export{p as default};
--- assets/useToc-liW28PZo.js
+++
@@ -1,1 +0,0 @@
-import{r as n}from"./index-KTs1BggA.js";const a=n([]),o=n([]),s=n(!1);function c(t){a.value=t,l()}function l(){o.value=[];let t="9";for(let e of a.value)t>e.tagName[1]&&(t=e.tagName[1]);for(let e of a.value)o.value.push({id:e.id,text:e.innerText,tab:parseInt(e.tagName[1])-parseInt(t)})}function u(){return o}const f=()=>({setToc:c,getToc:u,enableToc:s});export{f as u};
